06d87dee54c969435cf9fa540362ede130468eb0
Eynal Bakhshiyev <113455383+Eynal-01@users.noreply.github.com>
2024-08-07 22:42:47 +0400

    commit123

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,23 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'module:react-native-dotenv', // To use environment variables
+    // 'react-native-reanimated/plugin', // Required for react-native-reanimated
+    'nativewind/babel', // For using Tailwind CSS with React Native
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@stacks': './src/stacks',
+          '@screens': './src/screens',
+          '@common': './src/common',
+          '@utils': './src/utils',
+          '@icons': './assets/icons',
+          '@images': './assets/images',
+          '@locales': './src/locales',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/common/StyledComponents.jsx
@@ -0,0 +1,8 @@
+import {styled} from 'nativewind';
+import {View, Text, Button, TouchableOpacity, TextInput} from 'react-native';
+
+export const StyledView = styled(View);
+export const StyledText = styled(Text);
+export const StyledButton = styled(Button);
+export const StyledTouchableOpacity = styled(TouchableOpacity);
+export const StyledTextInput = styled(TextInput);
